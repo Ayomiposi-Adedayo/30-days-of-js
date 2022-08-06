b643b55dcf76b96729a76ba0f66b7092052c6779
Ayomiposi <perscey007@gmail.comm>
2022-08-06 23:02:03 +0100

    day 2 edit

--- day 2/main.js
+++ day 2/main.js
@@ -105,7 +105,7 @@ console.log(love.split('love').length - 1);
 let becauseNew = 'You cannot end a sentence with because because because is a conjunction'
 console.log(becauseNew.match(/because/g).length);
 // 3
-const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+// const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 // 4
 let annualIncome =  'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 let perAnnum = annualIncome.match(/\d+/g)
